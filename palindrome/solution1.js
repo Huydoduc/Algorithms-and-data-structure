974a394ad79a65698c36e11e08de2a1d76fe3503
DoDucHuy <doduchuy19981998@gmail.com>
2023-09-28 20:32:50 +0700

    the first solution for palindrome

---
+++ palindrome/solution1.js
@@ -0,0 +1,12 @@
+function palindrome(str) {
+  const reversed = str.split("").reverse().join("");
+  let result = "";
+  if (str === reversed) {
+    result = "this is a palindrome";
+  } else {
+    result = "this is not a palindrome";
+  }
+  return result;
+}
+
+console.log(palindrome("abba"));
