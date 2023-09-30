1e399c31c2d98f3a73f2bca2dac948ac1c646323
DoDucHuy <doduchuy19981998@gmail.com>
2023-09-30 20:48:07 +0700

    add comment to array chunk solution 1

--- array-chunking/solution1.js
+++ array-chunking/solution1.js
@@ -1,11 +1,19 @@
 function chunk(array, size) {
+  // create an empty array to store the resulting chunks
   let chunked = [];
+
+  // Iterate through each element in the input array
   for (element of array) {
+    //get the last chunk in the chunked array
     const last = chunked[chunked.length - 1];
 
+    //check if there is no last lunk or if it is already full (size elements)
     if (!last || last.length === size) {
+      // if either condition is true, create a new chunk (an empty array)
+      // and push the current element into it
       chunked.push([element]);
     } else {
+      // if the last chunk is not full, add the element to it
       last.push(element);
     }
   }
