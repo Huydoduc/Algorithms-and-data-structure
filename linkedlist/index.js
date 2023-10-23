c6e0d429adaf4e8ec7c7fd1880a2f58557bc39aa
DoDucHuy <doduchuy19981998@gmail.com>
2023-10-23 21:26:39 +0700

    linked list constructor

--- linkedlist/index.js
+++ linkedlist/index.js
@@ -9,6 +9,13 @@ class Node {
   }
 }
 
-class LinkedList {}
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+}
+
+const list = new LinkedList();
+list.head = new Node(10);
 
 module.exports = { Node, LinkedList };
